af1b32f46f1972e65cb4385364de58169ccd44ab
Gustavo Ivan Vega Olvera <gi.vega@itsur.edu.mx>
2023-03-17 12:04:53 -0600

    Se creo el proyecto base de super heroes, ya trae los recursos necesarios (imagenes y thema)

---
+++ SuperHeroesA/app/src/main/java/net/ivanvega/superheroesa/HeroesDataSource.kt
@@ -0,0 +1,4 @@
+package net.ivanvega.superheroesa
+
+class HeroesDataSource {
+}
---
+++ SuperHeroesA/app/src/main/java/net/ivanvega/superheroesa/model/Hero.kt
@@ -0,0 +1,7 @@
+package net.ivanvega.superheroesa.model
+
+data class Hero(
+    val nameRes: Int,
+    val descriptionRes: Int,
+    val imageRes: Int
+)
